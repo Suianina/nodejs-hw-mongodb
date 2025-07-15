8307bee4cdcfc134cfbc25921153b79161cfaeb9
Zhanna Suianina <cezyby3@gmail.com>
2025-07-15 15:48:20 +0300

    fix: GET, POST, PATCH request correction

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -2,6 +2,7 @@ import { model, Schema } from 'mongoose';
 
 import { emailRegex } from '../../constants/user.js';
 import { handleSaveError, setUpdateSettings } from './hooks.js';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema(
   {
@@ -29,5 +30,30 @@ userSchema.methods.toJSON = function () {
 userSchema.post('save', handleSaveError);
 userSchema.pre('findOneAndUpdate', setUpdateSettings);
 userSchema.post('findOneAndUpdate', handleSaveError);
+
+// Hash password before saving (for create and save)
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Hash password before findOneAndUpdate (for updateOne, findOneAndUpdate, etc.)
+userSchema.pre('findOneAndUpdate', async function (next) {
+  const update = this.getUpdate();
+  if (update && update.password) {
+    try {
+      update.password = await bcrypt.hash(update.password, 10);
+      this.setUpdate(update);
+    } catch (err) {
+      return next(err);
+    }
+  }
+  next();
+});
 
 export const UsersCollection = model('User', userSchema);
